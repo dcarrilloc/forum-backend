1491d7ff3cb7585db96786940ffb5068052c012b
Dani Carrillo <danielcarrillocardus@gmail.com>
2021-02-24 20:21:33 +0100

    Fixed img avatar suplier.

--- src/main/java/com/esliceu/forum/forum/controllers/ImageController.java
+++ src/main/java/com/esliceu/forum/forum/controllers/ImageController.java
@@ -1,9 +1,12 @@
 package com.esliceu.forum.forum.controllers;
 
 import com.esliceu.forum.forum.services.UserService;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +14,11 @@ public class ImageController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/images/users/{userid}")
+    @GetMapping(value = "/images/users/{userid}", produces = MediaType.IMAGE_JPEG_VALUE)
     public byte[] getUserImg(@PathVariable Long userid) {
-        System.out.println("Hola!");
-        return userService.getUserImage(userid);
+        byte[] userImg = userService.getUserImage(userid);
+        System.out.println(userImg);
+        String img64Bytes = new String(userImg).split(",")[1];
+        return Base64.decodeBase64(img64Bytes);
     }
 }
